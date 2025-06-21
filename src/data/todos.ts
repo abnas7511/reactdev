9eec60fb66eac6e2755150801696cb3b3c1f0dff
Muhammed Abnas <abnas7511@gmail.com>
2025-06-22 02:03:22 +0530

    first

---
+++ src/data/todos.ts
@@ -0,0 +1,28 @@
+import type { Todo } from "../types/todo";
+export const dummyData: Todo[] = [
+    {
+        id: 1,
+        title: "Learn React",
+        completed: false,
+    },
+    {
+        id: 2,
+        title: "Learn TypeScript",
+        completed: false,
+    },
+    {
+        id: 3,
+        title: "Build a Todo App",
+        completed: false,
+    },
+    {
+        id: 4,
+        title: "Deploy the App",
+        completed: false,
+    },
+    {
+        id: 5,
+        title: "Refactor Code",
+        completed: false,
+    },
+]
